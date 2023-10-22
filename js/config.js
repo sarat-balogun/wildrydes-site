d5e22b74f59224c46bfa626578b337f1ccff2dbc
sarat-balogun <saratbalogun6@gmail.com>
2023-10-22 23:15:53 +0100

    new_configuration

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'eu-west-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://1kbpzkjza7.execute-api.eu-north-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://7zyh4fc888.execute-api.eu-west-2.amazonaws.com/prod'
     }
 };
